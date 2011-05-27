fa6896b1257199a5fa6fc48561af77477a42bc90
TeAmigo <rickcharon@gmail.com>
2011-05-26 18:26:38 -0700

    Slowly making progress...

---
+++ navfile.js
@@ -0,0 +1,28 @@
+//Navigation File Created Thu May 26 18:21:00 2011
+
+document.write("<ul id=\"horiznav\"> \
+<li><a href=\"#\">notes\
+<ul>\
+<li><a href=\"http://localhost/TeAmigo/notes/SettingUpGitHubSite.html\">Setting Up My Github Site</a>\
+</li>\
+</ul>\
+</li>\
+<li><a href=\"#\">pts\
+<ul>\
+<li><a href=\"http://localhost/TeAmigo/pts/overview.html\">Overview</a>\
+</li>\
+<li><a href=\"http://localhost/TeAmigo/pts/charts.html\">ptscharts</a>\
+</li>\
+<li><a href=\"http://localhost/TeAmigo/pts/updater.html\">ptsupdater</a>\
+</li>\
+</ul>\
+</li>\
+<li><a href=\"#\">trading\
+<ul>\
+<li><a href=\"http://localhost/TeAmigo/trading/journal.html\">Journal</a>\
+</li>\
+</ul>\
+</li>\
+<li><a href=\"http://localhost/TeAmigo/index.html\">TeAmigo</a>\
+</li>\
+</ul>");
